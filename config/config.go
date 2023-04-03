520d7217e1efba5a8b2f4f194e13e8ae3712e304
Aybek <turk.883ar@gmail.com>
2023-04-03 12:01:41 -0700

    config

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type (
+	Config struct {
+		HTTP
+		Log
+	}
+
+	HTTP struct {
+		Port string `env:"PORT" env-default:"8080"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL" env-default:"Dev"`
+	}
+)
+
+func New() (*Config, error) {
+	cfg := &Config{}
+	if err := cleanenv.ReadEnv(cfg); err != nil {
+		return nil, fmt.Errorf("cannot read config: %w", err)
+	}
+
+	return cfg, nil
+
+}
