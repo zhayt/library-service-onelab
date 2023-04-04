95c47c54eba20d67d972323a53e4e1c97745705e
Aybek <turk.883ar@gmail.com>
2023-04-03 22:45:47 -0700

    change route: id in place of name

--- internal/transport/http/handler/router.go
+++ internal/transport/http/handler/router.go
@@ -9,11 +9,11 @@ func (h *Handler) InitRoute() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/users/", h.showAllUsers)
-	router.HandlerFunc(http.MethodGet, "/v1/users/:name", h.showUser)
+	router.HandlerFunc(http.MethodGet, "/v1/users/:id", h.showUser)
 	router.HandlerFunc(http.MethodPost, "/v1/users/", h.createUser)
-	router.HandlerFunc(http.MethodPut, "/v1/users/:name", h.updateCreateUser)
-	router.HandlerFunc(http.MethodPatch, "/v1/users/:name", h.updateUser)
-	router.HandlerFunc(http.MethodDelete, "/v1/users/:name", h.deleteUser)
+	router.HandlerFunc(http.MethodPut, "/v1/users/:id", h.updateCreateUser)
+	router.HandlerFunc(http.MethodPatch, "/v1/users/:id", h.updateUser)
+	router.HandlerFunc(http.MethodDelete, "/v1/users/:id", h.deleteUser)
 
 	return h.logRequest(h.secureHeaders(router))
 }
