9bd4e97d5db757822de66c5e4f3b4f2bfc1ca75c
Aybek <turk.883ar@gmail.com>
2023-04-11 05:43:07 -0700

    update: errors

--- internal/common/errors.go
+++ internal/common/errors.go
@@ -6,5 +6,10 @@ var (
 	ErrInvalidData   = errors.New("invalid data")
 	ErrNameTaken     = errors.New("name taken")
 	ErrUserNotExists = errors.New("user not exists")
+	ErrNoRows        = errors.New("empty table")
 	ErrEmptyField    = errors.New("empty field")
+)
+
+var (
+	ErrBookNotExists = errors.New("book not exists")
 )
