261e9a780a819739b4ec8dc124f9d4948141aa4f
Aybek <turk.883ar@gmail.com>
2023-04-11 05:29:50 -0700

    migrations

---
+++ internal/storage/postgres/migrations/000001_init.up.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS "user" (
+    id SERIAL PRIMARY KEY,
+    fio VARCHAR(70) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password char(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS book (
+    id serial PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    author VARCHAR(70) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS book_issue_history (
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    issue_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    return_date TIMESTAMP,
+    FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE
+);
