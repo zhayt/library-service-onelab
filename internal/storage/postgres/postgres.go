61cb763a9c2c61c8d5fea99b24db0f2bf8fb2048
Aybek <turk.883ar@gmail.com>
2023-04-11 05:30:35 -0700

    add: postgresql pool connection

---
+++ internal/storage/postgres/postgres.go
@@ -0,0 +1,28 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+const _timeOut = 5 * time.Second
+
+func NewConnectionPool(driver string, dsn string) (*sqlx.DB, error) {
+	db, err := sqlx.Open(driver, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("connot open db: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), _timeOut)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("cannot connect to db: %w", err)
+	}
+
+	return db, nil
+}
