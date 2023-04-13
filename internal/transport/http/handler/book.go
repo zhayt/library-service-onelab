1bba24e2a09a3bf8d8544335b592f9c71a020009
Aybek <turk.883ar@gmail.com>
2023-04-12 17:51:44 -0700

    add: new handlers ShowBook

--- internal/transport/http/handler/book.go
+++ internal/transport/http/handler/book.go
@@ -42,8 +42,35 @@ func (h *Handler) CreateBook(e echo.Context) error {
 }
 
 func (h *Handler) ShowBook(e echo.Context) error {
-	return nil
+	ctx, cancel := context.WithTimeout(e.Request().Context(), _timeoutContext)
+	defer cancel()
+
+	bookID, err := strconv.Atoi(e.Param("id"))
+	if err != nil {
+		h.log.Error("Param error", zap.Error(err))
+		return e.JSON(http.StatusNotFound, err)
+	}
+
+	book, err := h.book.GetBookByID(ctx, bookID)
+	if err != nil {
+		h.log.Error("Get book Id error", zap.Error(err))
+		// 500 or 404
+		return e.JSON(http.StatusNotFound, err)
+	}
+
+	h.log.Info("Book found", zap.Int("id", book.ID))
+	return e.JSON(http.StatusOK, book)
 }
+
+func (h *Handler) ShowAllBooks(e echo.Context) error {
+	ctx, cancel := context.WithTimeout(e.Request().Context(), _timeoutContext)
+	defer cancel()
+
+	books, err := h.book.GetAllBooks(ctx)
+	if err != nil {
+		h.log.Error("Get all books error", zap.Error(err))
+		return e.JSON(http.StatusInternalServerError, err)
+	}
 
 func (h *Handler) ShowBooks(e echo.Context) error {
 	return nil
