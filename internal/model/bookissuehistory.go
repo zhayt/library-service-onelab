3d2fa7fe9ca5d1a54a4ab7ceeff29076449a0af7
Aybek <turk.883ar@gmail.com>
2023-04-13 05:17:57 -0700

    add: new entity BorrowedBooks

--- internal/model/bookissuehistory.go
+++ internal/model/bookissuehistory.go
@@ -6,6 +6,14 @@ type BIHistory struct {
 	ID         int       `json:"id"`
 	BookID     int       `json:"bookID"`
 	UserID     int       `json:"userID"`
-	IssueDate  time.Time `json:"issueDate"`
+	CreatedAt  time.Time `json:"issueDate"`
 	ReturnDate time.Time `json:"returnDate"`
+}
+
+type BorrowedBooks struct {
+	ID         int       `json:"id"`
+	UserName   string    `json:"userName" db:"fio"`
+	BookName   string    `json:"bookName" db:"name"`
+	BookAuthor string    `json:"bookAuthor" db:"author"`
+	CreatedAt  time.Time `json:"issueDate" db:"created_at"`
 }
