d9734194fda049a50791dd7fd4a7b5a6f5d0b563
Aybek <turk.883ar@gmail.com>
2023-04-18 02:39:45 -0700

    update: entity BIHistory has new field RentalBooks instead BookID

--- internal/model/bookissuehistory.go
+++ internal/model/bookissuehistory.go
@@ -3,11 +3,16 @@ package model
 import "time"
 
 type BIHistory struct {
-	ID         int       `json:"id"`
-	BookID     int       `json:"bookID"`
-	UserID     int       `json:"userID"`
-	CreatedAt  time.Time `json:"issueDate"`
-	ReturnDate time.Time `json:"returnDate"`
+	ID         int            `json:"id"`
+	Books      []*RentalBooks `json:"bookID"`
+	UserID     int            `json:"userID"`
+	CreatedAt  time.Time      `json:"issueDate"`
+	ReturnDate time.Time      `json:"returnDate"`
+}
+
+type RentalBooks struct {
+	ID       int `json:"ID"`
+	Quantity int `json:"quantity"`
 }
 
 type BorrowedBooks struct {
@@ -15,5 +20,6 @@ type BorrowedBooks struct {
 	UserName   string    `json:"userName" db:"fio"`
 	BookName   string    `json:"bookName" db:"name"`
 	BookAuthor string    `json:"bookAuthor" db:"author"`
+	Quantity   int       `json:"quantity" db:"quantity"`
 	CreatedAt  time.Time `json:"issueDate" db:"created_at"`
 }
