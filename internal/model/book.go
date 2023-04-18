c43052fbd7e03dd0546fa42405747fed097affc4
Aybek <turk.883ar@gmail.com>
2023-04-18 02:38:49 -0700

    update: add new price field into book entity

--- internal/model/book.go
+++ internal/model/book.go
@@ -1,7 +1,8 @@
 package model
 
 type Book struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Author string `json:"author"`
+	ID     int     `json:"id"`
+	Name   string  `json:"name"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
 }
