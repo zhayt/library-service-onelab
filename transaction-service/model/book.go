ee7b71a0536adf0ff9f7a22f4d1500202c0e1e5a
Aybek <turk.883ar@gmail.com>
2023-04-20 03:59:16 -0700

    initial commit

--- transaction-service/model/book.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Book struct {
-	ID     uint    `json:"id"`
-	Title  string  `json:"title"`
-	Author string  `json:"author"`
-	Price  float64 `json:"price"`
-}
--- transaction-service/storage/postgres/migrations/000001_init.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE transaction_item;
-
-DROP TABLE transaction;
