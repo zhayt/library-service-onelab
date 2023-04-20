9c43c8578f7a24a50149d2caa8355219da6a06ff
Aybek <turk.883ar@gmail.com>
2023-04-20 05:09:03 -0700

    update transaction method

--- internal/service/transaction.go
+++ internal/service/transaction.go
@@ -4,34 +4,40 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/zhayt/user-storage-service/internal/model"
 	"go.uber.org/zap"
 	"net/http"
+	"strconv"
 )
 
 type Transaction struct {
-	url string
-	log *zap.Logger
+	transactionURL string
+	itemURL        string
+	deleteURL      string
+	log            *zap.Logger
 }
 
 func NewTransaction(log *zap.Logger) *Transaction {
 	return &Transaction{
-		url: "localhost:8081/api/v1",
-		log: log}
+		transactionURL: "http://localhost:8081/api/v1/transactions",
+		itemURL:        "http://localhost:8081/api/v1/transactions/items",
+		deleteURL:      "http://localhost:8081/api/v1/transactions/",
+		log:            log}
 }
 
 func (s *Transaction) CreateTransaction(transaction model.Transaction) (int, error) {
-	req, err := http.NewRequest(http.MethodPost, s.url, bytes.NewBuffer([]byte("")))
-	if err != nil {
-		return 0, err
-	}
+	jsonData, _ := json.Marshal(transaction)
 
-	req.Header.Set("Content-Type", "application/json")
+	req, err := prepareRequest(http.MethodPost, s.transactionURL, jsonData)
+	if err != nil {
+		return 0, fmt.Errorf("pepare request error: %w", err)
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return 0, fmt.Errorf("do request error: %w", err)
 	}
 	defer resp.Body.Close()
 
@@ -50,9 +56,52 @@ func (s *Transaction) CreateTransaction(transaction model.Transaction) (int, err
 }
 
 func (s *Transaction) CreateTransactionItem(item model.TransactionItem) error {
-	return nil
+	jsonData, _ := json.Marshal(item)
+
+	req, err := prepareRequest(http.MethodPost, s.itemURL, jsonData)
+	if err != nil {
+		return fmt.Errorf("pepare request error: %w", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("do request error: %w", err)
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		return nil
+	} else {
+		return errors.New("transaction service error")
+	}
 }
 
 func (s *Transaction) DeleteTransaction(transactionID int) error {
-	return nil
+	req, err := prepareRequest(http.MethodDelete, s.deleteURL+strconv.Itoa(transactionID), []byte{})
+	if err != nil {
+		return fmt.Errorf("pepare request error: %w", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("do request error: %w", err)
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		return nil
+	} else {
+		return errors.New("transaction service error")
+	}
+}
+
+func prepareRequest(method string, url string, data []byte) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	return req, nil
 }
